9fd26ce5cd17fb0a2d5b3a8571f5944b3485cbc4
Brianna McNeil <briannamcneil.dev@gmail.com>
2025-10-20 11:31:28 -0400

    scale model

--- doremi-csharp/Models/Scale.cs
+++ doremi-csharp/Models/Scale.cs
@@ -7,14 +7,15 @@ public class Scale
         Major,
         RelativeMinor
     }
-    public string Root { get; init; }
-    public ScaleType Type { get; init; }
-    public IReadOnlyList<string> Notes { get; init; }
+
+    public string Root { get; private set; }
+    public ScaleType Type { get; private set; }
+    public IReadOnlyList<string> Notes { get; private set; }
 
     public Scale(string root, ScaleType type, IReadOnlyList<string> notes)
     {
         Root = root;
-        Type = type
+        Type = type;
         Notes = notes;
     }
 }
