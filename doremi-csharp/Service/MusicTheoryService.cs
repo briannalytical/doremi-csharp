17c240c2812d94c38f08a9dbc4340f84ae53e6d7
Brianna McNeil <briannamcneil.dev@gmail.com>
2025-11-15 08:47:43 -0500

    skeleton for service

--- doremi-csharp/Service/MusicTheoryService.cs
+++ doremi-csharp/Service/MusicTheoryService.cs
@@ -1,25 +1,28 @@
-using doremi_csharp.Interface;
 using doremi_csharp.Models;
+using System.Collections.Generic;
+using doremi_csharp.Interface;
+using doremi_csharp.Service;
 
-namespace doremi_csharp.Service
+namespace doremi_csharp.Services
 {
     public class MusicTheoryService : IMusicTheoryService
     {
+        private readonly ChromaticScale _chromaticScale;
+        
+        public MusicTheoryService()
+        {
+            _chromaticScale = new ChromaticScale();
+        }
+
         public Scale BuildScale(string rootNote, ScaleType scaleType)
         {
-            try
-            {
-
-            }
-            catch
-            {
-                throw new NotImplementedException();
-            }
+            // TODO: Implement scale building
+            throw new NotImplementedException();
         }
 
         public Chord BuildChord(string rootNote, ChordType chordType)
         {
-            // TODO: Build the chord
+            // TODO: Implement chord building
             throw new NotImplementedException();
         }
     }
